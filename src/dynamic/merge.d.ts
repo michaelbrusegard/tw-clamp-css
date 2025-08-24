11ba56f82dab89a8b7bcdd353462329afae3f400
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-08-24 19:24:06 +0200

    Revert "simplify type definitions for tailwind-merge"
    This reverts commit 3c8a67ff72fb83fc8bdba000d76394be99369860.

--- src/dynamic/merge.d.ts
+++ src/dynamic/merge.d.ts
@@ -1,7 +1,9 @@
 import type { Config } from "tailwind-merge";
 
-declare function withClamp(
-  prevConfig: Config,
-): Config;
+export declare function withClamp<
+  ClassGroupIds extends string,
+  ThemeGroupIds extends string,
+>(
+  prevConfig: Config<ClassGroupIds, ThemeGroupIds>,
+): Config<ClassGroupIds, ThemeGroupIds>;
 
-export default withClamp;
--- src/static/merge.d.ts
+++ src/static/merge.d.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwind-merge";
 
-declare function withClamp(
-  prevConfig: Config,
-): Config;
-
-export default withClamp;
+export declare function withClamp<
+  ClassGroupIds extends string,
+  ThemeGroupIds extends string,
+>(
+  prevConfig: Config<ClassGroupIds, ThemeGroupIds>,
+): Config<ClassGroupIds, ThemeGroupIds>;
