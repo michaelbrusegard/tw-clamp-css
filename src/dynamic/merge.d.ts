3c8a67ff72fb83fc8bdba000d76394be99369860
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-08-24 19:19:55 +0200

    simplify type definitions for tailwind-merge

--- src/dynamic/merge.d.ts
+++ src/dynamic/merge.d.ts
@@ -1,9 +1,7 @@
 import type { Config } from "tailwind-merge";
 
-export declare function withClamp<
-  ClassGroupIds extends string,
-  ThemeGroupIds extends string,
->(
-  prevConfig: Config<ClassGroupIds, ThemeGroupIds>,
-): Config<ClassGroupIds, ThemeGroupIds>;
+declare function withClamp(
+  prevConfig: Config,
+): Config;
 
+export default withClamp;
